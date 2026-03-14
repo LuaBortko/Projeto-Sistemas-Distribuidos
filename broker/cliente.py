40ed04f438753c36d2e5010091e3b72164b8b72f
Luana <luanabortko2004@gmail.com>
2026-03-14 19:39:38 -0300

    funcionalidade de login, entrar, criar e listar canais

--- broker/cliente.py
+++ broker/cliente.py
@@ -1,14 +1,35 @@
 import zmq
 from time import sleep
+from datetime import datetime
+import zoneinfo
 
 context = zmq.Context()
 socket = context.socket(zmq.REQ)
 socket.connect("tcp://broker:5555")
+fuso = zoneinfo.ZoneInfo("America/Sao_Paulo")
 
 
-msg = "login usuario1"
-print(f"Requerimento de Login do usuario: {msg}", flush=True)
-socket.send_string(msg)  # Enviando a mensagem para o servidor
-recebido = socket.recv()  # Mensagem recebida do servidor
-print(f"Resposta do servidor:{recebido}", flush=True)
-sleep(1)
+def mandar(msg):
+    tempo = datetime.now(tz=fuso).strftime("%H:%M")
+    print(f"Mensagem do usuario em {tempo}. Mensagem: {msg}", flush=True)
+    socket.send_string(msg)  # Enviando a mensagem para o servidor
+
+
+while True:
+    mandar("login usuario1 -")
+    recebido = socket.recv()  # Mensagem recebida do servidor
+    print(f"Resposta do servidor: {recebido}", flush=True)
+    # if recebido == "success":
+    sleep(2)
+    mandar("entrar usuario1 teste1")
+    recebido = socket.recv()
+    print(f"Resposta do servidor: {recebido}", flush=True)
+    sleep(2)
+    mandar("listar usuario2 -")
+    recebido = socket.recv()
+    print(f"Resposta do servidor: {recebido}", flush=True)
+    sleep(2)
+    mandar("listar usuario1 -")
+    recebido = socket.recv()
+    print(f"Resposta do servidor: {recebido}", flush=True)
+    sleep(4)
--- broker/servidor.py
+++ broker/servidor.py
@@ -1,11 +1,56 @@
 import zmq
+from time import sleep
+from datetime import datetime
+import zoneinfo
 
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.connect("tcp://broker:5556")
+fuso = zoneinfo.ZoneInfo("America/Sao_Paulo")
 
 usuarios = list()
+usuariosLogados = list()
+canais = list()
 
 while True:
     msg = socket.recv_string()
-    funcao, resto = msg.split()
+    funcao, user, canal = msg.split()
+    tempo = datetime.now(tz=fuso).strftime("%H:%M")
+    if funcao == "login":
+        if user in usuariosLogados:
+            socket.send_string("err2")
+            print(
+                f"Erro ao entrar no servidor as {tempo}, nome ja existente", flush=True)
+        else:
+            if user not in usuarios:
+                usuarios.append(user)
+            usuariosLogados.append(user)
+            socket.send_string("success")
+            print(
+                f"O usuario {user} entrou no servidor com sucesso as {tempo}", flush=True)
+    elif funcao == "entrar":
+        if user not in usuariosLogados:
+            socket.send_string("err3")
+            print(
+                f"O usuario {user} não esta logado, tentativa de acesso as {tempo}", flush=True)
+        else:
+            if canal not in canais:
+                canais.append(canal)
+                socket.send_string("success")
+                print(
+                    f"Canal não encontrado, criado novo canal com o nome {canal} as {tempo}", flush=True)
+            else:
+                socket.send_string("success")
+                print(f"Entrou no canal {canal} com sucesso! as {tempo}")
+    elif funcao == "listar":
+        if user not in usuariosLogados:
+            socket.send_string("err3")
+            print(
+                f"O usuario {user} não esta logado, tentativa de acesso as {tempo}", flush=True)
+        else:
+            socket.send_string("success")
+            print(canais)
+
+    else:
+        socket.send_string("err1")
+        print(f"Comando não reconhecido as {tempo}", flush=True)
